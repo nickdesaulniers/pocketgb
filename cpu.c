bf176373be94788a9637a7ef474f66a12d404ae5
Nick Desaulniers <nick.desaulniers@gmail.com>
2018-02-22 22:34:41 -0800

    fix up ret and rl

--- cpu.c
+++ cpu.c
@@ -56,7 +56,7 @@ static void CB_RL_C (struct cpu* const lr35902) {
   PBYTE(6, lr35902->registers.c);
 }
 
-static void CB_SWAP (struct cpu* const lr35902) {
+static void CB_SWAP_A (struct cpu* const lr35902) {
   LOG(5, "SWAP\n");
   const uint8_t x = lr35902->registers.a;
   lr35902->registers.a = (x & 0x0F) << 4 | (x & 0xF0) >> 4;
@@ -66,11 +66,21 @@ static void CB_SWAP (struct cpu* const lr35902) {
   lr35902->registers.f.c = 0;
 }
 
+static void CB_SRL_A (struct cpu* const lr35902) {
+  LOG(5, "SRL A\n");
+  const uint8_t old_bit_0 = lr35902->registers.a & 0x01;
+  lr35902->registers.a >>= 1;
+  lr35902->registers.f.z = !lr35902->registers.a;
+  lr35902->registers.f.n = 0;
+  lr35902->registers.f.h = 0;
+  lr35902->registers.f.c = old_bit_0;
+}
+
 static const instr cb_opcodes [256] = {
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 0x
   0, CB_RL_C, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 1x
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 2x
-  0, 0, 0, 0, 0, 0, 0, CB_SWAP, 0, 0, 0, 0, 0, 0, 0, 0, // 3x
+  0, 0, 0, 0, 0, 0, 0, CB_SWAP_A, 0, 0, 0, 0, 0, 0, 0, CB_SRL_A, // 3x
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 4x
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 5x
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 6x
@@ -90,6 +100,12 @@ instr decode_cb (const struct cpu* const cpu) {
   uint8_t opcode = rb(cpu->mmu, pc);
   PBYTE(4, opcode);
   instr i = cb_opcodes[opcode];
+#ifndef NDEBUG
+  if (i == 0) {
+    fprintf(stderr, "Unknown opcode for CB PREFIX " PRIbyte " @ " PRIshort "\n",
+        opcode, pc);
+  }
+#endif
   assert(i != 0);
   return i;
 }
@@ -123,6 +139,7 @@ static void INC_ ## X (struct cpu* const lr35902) { \
   ++REG(pc); \
 }
 
+INC_x(a, A);
 INC_x(b, B);
 INC_x(c, C);
 INC_x(h, H);
@@ -220,14 +237,18 @@ static void ADD_A_d8 (struct cpu* const lr35902) {
   lr35902->registers.pc += 2;
 }
 
-static void ADD_HL_DE (struct cpu* const lr35902) {
-  LOG(5, "ADD HL,DE\n");
-  lr35902->registers.hl += lr35902->registers.de;
-  lr35902->registers.f.n = 0;
-  lr35902->registers.f.h = (lr35902->registers.hl & 0x10) == 0x10;
-  lr35902->registers.f.c = (lr35902->registers.hl & 0x80) != 0;
-  ++lr35902->registers.pc;
+#define ADD_xy_zw(xy, zw, XY, ZW) \
+static void ADD_ ## XY ## _ ## ZW (struct cpu* const lr35902) { \
+  LOG(5, "ADD " #XY "," #ZW "\n"); \
+  REG(xy) += REG(zw); \
+  lr35902->registers.f.n = 0; \
+  lr35902->registers.f.h = (REG(xy) & 0x10) == 0x10; \
+  lr35902->registers.f.c = (REG(xy) & 0x80) != 0; \
+  ++REG(pc); \
 }
+
+ADD_xy_zw(hl, de, HL, DE);
+ADD_xy_zw(hl, hl, HL, HL);
 
 static void ADC_A_d8 (struct cpu* const lr35902) {
   LOG(5, "ADC A,d8\n");
@@ -283,17 +304,21 @@ LD_x_y(REG(a), REG(e), A, E);
 LD_x_y(REG(a), REG(h), A, H);
 LD_x_y(REG(a), REG(l), A, L);
 LD_x_y(REG(b), REG(a), B, A);
+LD_x_y(REG(b), REG(b), B, B);
 LD_x_y(REG(c), REG(a), C, A);
 LD_x_y(REG(d), REG(a), D, A);
 LD_x_y(REG(e), REG(a), E, A);
 LD_x_y(REG(h), REG(a), H, A);
+LD_x_y(REG(h), REG(b), H, B);
 LD_x_y(REG(l), REG(a), L, A);
-LD_x_y(REG(sp), REG(hl), SP, HL)
-LD_x_y(REG(a), DEREF_READ(de), A, DEREF_DE)
-LD_x_y(REG(d), DEREF_READ(hl), D, DEREF_HL)
-LD_x_y(REG(e), DEREF_READ(hl), E, DEREF_HL)
-LD_x_y(REG(l), DEREF_READ(hl), L, DEREF_HL)
-/*LD_x_y(DEREF_WRITE(hl), REG(a), DEREF_HL, A)*/
+LD_x_y(REG(sp), REG(hl), SP, HL);
+LD_x_y(REG(a), DEREF_READ(de), A, DEREF_DE);
+LD_x_y(REG(a), DEREF_READ(hl), A, DEREF_HL);
+LD_x_y(REG(d), DEREF_READ(hl), D, DEREF_HL);
+LD_x_y(REG(e), DEREF_READ(hl), E, DEREF_HL);
+LD_x_y(REG(h), DEREF_READ(hl), H, DEREF_HL);
+LD_x_y(REG(l), DEREF_READ(hl), L, DEREF_HL);
+/*LD_x_y(DEREF_WRITE(hl), REG(a), DEREF_HL, A);*/
 
 static void LD_DEREF_HL_A (struct cpu* const lr35902) {
   LOG(5, "LD (HL),A\n");
@@ -398,6 +423,7 @@ LD_x_d8(REG(b), B);
 LD_x_d8(REG(c), C);
 LD_x_d8(REG(d), D);
 LD_x_d8(REG(e), E);
+LD_x_d8(REG(h), H);
 LD_x_d8(REG(l), L);
 
 #define LD_xy_d16(xy, XY) \
@@ -442,6 +468,12 @@ static void LD_DEREF_a16_A (struct cpu* const lr35902) {
   lr35902->registers.pc += 3;
 }
 
+static void LD_DEREF_BC_A (struct cpu* const lr35902) {
+  LOG(5, "LD (BC),A\n");
+  wb(lr35902->mmu, REG(bc), REG(a));
+  ++REG(pc);
+}
+
 #define PUSH_xy(xy, XY) \
 static void PUSH_ ## XY (struct cpu* const lr35902) { \
   LOG(5, "PUSH " #XY "\n"); \
@@ -465,6 +497,7 @@ static void POP_ ## XY (struct cpu* const lr35902) { \
 
 POP_xy(af, AF);
 POP_xy(bc, BC);
+POP_xy(de, DE);
 POP_xy(hl, HL);
 
 static void __JR_r8 (struct cpu* const lr35902) {
@@ -528,6 +561,15 @@ static void JP_a16 (struct cpu* const lr35902) {
   lr35902->registers.pc = a16;
 }
 
+static void JP_NZ_a16 (struct cpu* const lr35902) {
+  LOG(5, "JP NZ,a16\n");
+  if (lr35902->registers.f.z) {
+    REG(pc) += 3;
+  } else {
+    REG(pc) = load_d16(lr35902);
+  }
+}
+
 static void JP_DEREF_HL (struct cpu* const lr35902) {
   LOG(5, "JP (HL)\n");
   lr35902->registers.pc = rw(lr35902->mmu, lr35902->registers.hl);
@@ -576,14 +618,26 @@ static void RET (struct cpu* const lr35902) {
 
 static void RET_Z (struct cpu* const lr35902) {
   LOG(5, "RET Z\n");
-  // pop 2B from stack
-  const uint16_t a = rw(lr35902->mmu, lr35902->registers.sp);
-  PSHORT(6, a);
-  lr35902->registers.sp += 2;
   if (lr35902->registers.f.z) {
+    // pop 2B from stack
+    lr35902->registers.sp += 2;
     // jump to that address
     LOG(6, "jumping\n");
-    lr35902->registers.pc = a;
+    lr35902->registers.pc = rw(lr35902->mmu, lr35902->registers.sp);
+  } else {
+    LOG(6, "not jumping\n");
+    ++lr35902->registers.pc;
+  }
+}
+
+static void RET_C (struct cpu* const lr35902) {
+  LOG(5, "RET C\n");
+  if (lr35902->registers.f.c) {
+    // pop 2B from stack
+    lr35902->registers.sp += 2;
+    // jump to that address
+    LOG(6, "jumping\n");
+    lr35902->registers.pc = rw(lr35902->mmu, lr35902->registers.sp);
   } else {
     LOG(6, "not jumping\n");
     ++lr35902->registers.pc;
@@ -592,16 +646,15 @@ static void RET_Z (struct cpu* const lr35902) {
 
 static void RET_NC (struct cpu* const lr35902) {
   LOG(5, "RET NC\n");
-  // pop 2B from stack
-  const uint16_t a = rw(lr35902->mmu, lr35902->registers.sp);
-  PSHORT(6, a);
-  lr35902->registers.sp += 2;
-  if (!lr35902->registers.f.c) {
+  if (lr35902->registers.f.c) {
+    LOG(6, "not jumping\n");
+    ++lr35902->registers.pc;
+  } else {
+    // pop 2B from stack
+    lr35902->registers.sp += 2;
     // jump to that address
     LOG(6, "jumping\n");
-    lr35902->registers.pc = a;
-  } else {
-    LOG(6, "not jumping\n");
+    lr35902->registers.pc = rw(lr35902->mmu, lr35902->registers.sp);
   }
 }
 
@@ -616,24 +669,37 @@ static void RST_28 (struct cpu* const lr35902) {
 
 static void RLA (struct cpu* const lr35902) {
   LOG(5, "RLA\n");
-  const uint8_t n = lr35902->registers.a;
-  const uint8_t r = (n << 1) | lr35902->registers.f.c;
+  const uint8_t old_7_bit = lr35902->registers.a & 0x80;
+  PBYTE(6, lr35902->registers.a);
+  lr35902->registers.a <<= 1;
+  lr35902->registers.a |= lr35902->registers.f.c;
   PBYTE(6, lr35902->registers.a);
   lr35902->registers.f.z = 0;
   lr35902->registers.f.n = 0;
   lr35902->registers.f.h = 0;
-  lr35902->registers.f.c = (n & 0x80) != 0;
-  lr35902->registers.a = r;
+  lr35902->registers.f.c = old_7_bit;
+  ++lr35902->registers.pc;
+}
+
+static void RLCA (struct cpu* const lr35902) {
+  LOG(5, "RLCA\n");
+  const uint8_t old_7_bit = lr35902->registers.a & 0x80;
   PBYTE(6, lr35902->registers.a);
+  lr35902->registers.a <<= 1;
+  PBYTE(6, lr35902->registers.a);
+  lr35902->registers.f.z = 0;
+  lr35902->registers.f.n = 0;
+  lr35902->registers.f.h = 0;
+  lr35902->registers.f.c = old_7_bit;
   ++lr35902->registers.pc;
 }
 
 static void RRA (struct cpu* const lr35902) {
   LOG(5, "RRA\n");
   const uint8_t old_0_bit = lr35902->registers.a & 0x01;
-  const uint8_t old_carry_bit = lr35902->registers.f.c;
   PBYTE(6, lr35902->registers.a);
-  lr35902->registers.a = (lr35902->registers.a >> 1) | (old_carry_bit << 7);
+  lr35902->registers.a >>= 1;
+  lr35902->registers.a |= lr35902->registers.f.c << 7;
   lr35902->registers.f.z = !lr35902->registers.a;
   lr35902->registers.f.n = 0;
   lr35902->registers.f.h = 0;
@@ -698,6 +764,16 @@ OR_x(REG(b), B);
 OR_x(REG(c), C);
 OR_x(DEREF_READ(hl), DEREF_HL);
 
+static void OR_d8 (struct cpu* const lr35902) {
+  LOG(5, "OR d8\n");
+  REG(a) |= load_d8(lr35902);
+  lr35902->registers.f.z = !lr35902->registers.a;
+  lr35902->registers.f.n = 0;
+  lr35902->registers.f.h = 0;
+  lr35902->registers.f.c = 0;
+  ++REG(pc);
+}
+
 static void AND_C (struct cpu* const lr35902) {
   LOG(5, "AND C\n");
   lr35902->registers.a &= lr35902->registers.c;
@@ -730,22 +806,22 @@ static void CPL (struct cpu* const lr35902) {
 }
 
 static const instr opcodes [256] = {
-  NOP, LD_BC_d16, 0, INC_BC, INC_B, DEC_B, LD_B_d8, 0, 0, 0, 0, DEC_BC, INC_C, DEC_C, LD_C_d8, 0, // 0x
+  NOP, LD_BC_d16, LD_DEREF_BC_A, INC_BC, INC_B, DEC_B, LD_B_d8, RLCA, 0, 0, 0, DEC_BC, INC_C, DEC_C, LD_C_d8, 0, // 0x
   0, LD_DE_d16, 0, INC_DE, 0, DEC_D, LD_D_d8, RLA, JR_r8, ADD_HL_DE, LD_A_DEREF_DE, 0, 0, DEC_E, LD_E_d8, RRA, // 1x
-  JR_NZ_r8, LD_HL_d16, LD_DEREF_HL_INC_A, INC_HL, INC_H, 0, 0, 0, JR_Z_r8, 0, LD_A_DEREF_HL_INC, 0, INC_L, DEC_L, LD_L_d8, CPL, // 2x
-  JR_NC_r8, LD_SP_d16, LD_DEREF_HL_DEC_A, 0, 0, DEC_DEREF_HL, LD_DEREF_HL_d8, 0, 0, 0, 0, 0, 0, DEC_A, LD_A_d8, 0, // 3x
-  0, 0, 0, 0, 0, 0, 0, LD_B_A, 0, 0, 0, 0, 0, 0, 0, LD_C_A, // 4x
+  JR_NZ_r8, LD_HL_d16, LD_DEREF_HL_INC_A, INC_HL, INC_H, 0, LD_H_d8, 0, JR_Z_r8, ADD_HL_HL, LD_A_DEREF_HL_INC, 0, INC_L, DEC_L, LD_L_d8, CPL, // 2x
+  JR_NC_r8, LD_SP_d16, LD_DEREF_HL_DEC_A, 0, 0, DEC_DEREF_HL, LD_DEREF_HL_d8, 0, 0, 0, 0, 0, INC_A, DEC_A, LD_A_d8, 0, // 3x
+  LD_B_B, 0, 0, 0, 0, 0, 0, LD_B_A, 0, 0, 0, 0, 0, 0, 0, LD_C_A, // 4x
   0, 0, 0, 0, 0, 0, LD_D_DEREF_HL, LD_D_A, 0, 0, 0, 0, 0, 0, LD_E_DEREF_HL, LD_E_A, // 5x
-  0, 0, 0, 0, 0, 0, 0, LD_H_A, 0, 0, 0, 0, 0, 0, LD_L_DEREF_HL, LD_L_A, // 6x
-  0, 0, 0, 0, 0, 0, 0, LD_DEREF_HL_A, LD_A_B, LD_A_C, LD_A_D, LD_A_E, LD_A_H, LD_A_L, 0, 0, // 7x
+  LD_H_B, 0, 0, 0, 0, 0, LD_H_DEREF_HL, LD_H_A, 0, 0, 0, 0, 0, 0, LD_L_DEREF_HL, LD_L_A, // 6x
+  0, 0, 0, 0, 0, 0, 0, LD_DEREF_HL_A, LD_A_B, LD_A_C, LD_A_D, LD_A_E, LD_A_H, LD_A_L, LD_A_DEREF_HL, 0, // 7x
   0, 0, 0, 0, 0, 0, ADD_A_DEREF_HL, ADD_A_A, 0, 0, 0, 0, 0, 0, 0, 0, // 8x
   SUB_B, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 9x
   0, AND_C, 0, 0, 0, 0, 0, 0, 0, XOR_C, 0, 0, 0, 0, 0, XOR_A, // Ax
   OR_B, OR_C, 0, 0, 0, 0, OR_DEREF_HL, OR_A, 0, 0, 0, 0, 0, 0, CP_DEREF_HL, 0, // Bx
-  0, POP_BC, 0, JP_a16, CALL_NZ_a16, PUSH_BC, ADD_A_d8, 0, RET_Z, RET, 0, handle_cb, 0, CALL_a16, ADC_A_d8, 0, // Cx
-  RET_NC, 0, 0, 0, 0, PUSH_DE, SUB_d8, 0, 0, 0, 0, 0, 0, 0, 0, 0, // Dx
+  0, POP_BC, JP_NZ_a16, JP_a16, CALL_NZ_a16, PUSH_BC, ADD_A_d8, 0, RET_Z, RET, 0, handle_cb, 0, CALL_a16, ADC_A_d8, 0, // Cx
+  RET_NC, POP_DE, 0, 0, 0, PUSH_DE, SUB_d8, 0, RET_C, 0, 0, 0, 0, 0, 0, 0, // Dx
   LDH_DEREF_a8_A, POP_HL, LD_DEREF_C_A, 0, 0, PUSH_HL, AND_d8, 0, 0, JP_DEREF_HL, LD_DEREF_a16_A, 0, 0, 0, 0, RST_28, // Ex
-  LDH_A_DEREF_a8, POP_AF, 0, DI, 0, PUSH_AF, 0, 0, LD_HL_SP_r8, LD_SP_HL, LD_A_DEREF_a16, EI, 0, 0, CP_d8, 0  // Fx
+  LDH_A_DEREF_a8, POP_AF, 0, DI, 0, PUSH_AF, OR_d8, 0, LD_HL_SP_r8, LD_SP_HL, LD_A_DEREF_a16, EI, 0, 0, CP_d8, 0  // Fx
 };
 
 instr decode (const struct cpu* const cpu) {
