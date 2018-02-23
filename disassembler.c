73b71b15614b2fbf3df5a4fa41eedc552b3ca3dd
Nick Desaulniers <nick.desaulniers@gmail.com>
2018-02-22 23:05:47 -0800

    Revert "disassembler: add interleaved instruction table"
    This reverts commit a3eabd03281f685feb5a444842ed14b1b4d10a77.

--- disassembler.c
+++ disassembler.c
@@ -104,7 +104,6 @@ enum __attribute__((packed)) Opcode {
   kJR,
   kCALL,
   kRET,
-  kRETI,
   kRST,
   // Interrupts
   kEI,
@@ -151,7 +150,6 @@ static const char* const opcode_str_table [] = {
   "JR",
   "CALL",
   "RET",
-  "RETI",
   "RST",
   "EI",
   "DI",
@@ -578,171 +576,6 @@ struct instruction {
   uint8_t length;
 };
 
-static const struct instruction instruction_table [256] = {
-  // 0x
-  { kNOP, { kNONE, kNONE }, 2 }, { kLD, { kBC, kd16 }, 3 },
-  { kLD, { kDEREF_BC, kA }, 1 }, { kINC, { kBC, kNONE }, 1 },
-  { kINC, { kB, kNONE }, 1 }, { kDEC, { kB, kNONE }, 1 },
-  { kLD, { kB, kd8 }, 2 }, { kRLC, { kA, kNONE }, 1 },
-
-  { kLD, { kDEREF_a16, kSP }, 3 }, { kADD, { kHL, kBC }, 1 },
-  { kLD, { kA, kDEREF_BC }, 1 }, { kDEC, { kBC, kNONE }, 1 },
-  { kINC, { kC, kNONE }, 1 }, { kDEC, { kC, kNONE }, 1 },
-  { kLD, { kC, kd8 }, 2 }, { kRRC, { kA, kNONE }, 1 },
-  // 1x
-  { kSTOP, { kNONE, kNONE }, 2 }, { kLD, { kDE, kd16 }, 3 },
-  { kLD, { kDEREF_DE, kA }, 1 }, { kINC, { kDE, kNONE }, 1 },
-  { kINC, { kD, kNONE }, 1 }, { kDEC, { kD, kNONE }, 1 },
-  { kLD, { kD, kd8 }, 2 }, { kRL, { kA, kNONE }, 1 },
-
-  { kJR, { kNONE, kr8 }, 2 }, { kADD, { kHL, kDE }, 1 },
-  { kLD, { kA, kDEREF_DE }, 1 }, { kDEC, { kDE, kNONE }, 1 },
-  { kINC, { kE, kNONE }, 1 }, { kDEC, { kE, kNONE }, 1},
-  { kLD, { kE, kd8 }, 2 }, { kRR, { kA, kNONE}, 1 },
-  // 2x
-  { kJR, { kCOND_NZ, kr8 }, 2 }, { kLD, { kHL, kd16 }, 3 },
-  { kLD, { kDEREF_HL_INC, kA }, 1 }, { kINC, { kHL, kNONE }, 1 },
-  { kINC, { kH, kNONE }, 1 }, { kDEC, { kH, kNONE }, 1 },
-  { kLD, { kH, kd8 }, 2 }, { kDAA, { kNONE, kNONE }, 1 },
-
-  { kJR, { kCOND_Z, kr8 }, 2 }, { kADD, { kHL, kHL }, 1 },
-  { kLD, { kA, kDEREF_HL_INC }, 1 }, { kDEC, { kHL, kNONE }, 1 },
-  { kINC, { kL, kNONE }, 1 }, { kDEC, { kL, kNONE }, 1 },
-  { kLD, { kL, kd8 }, 2 }, { kCPL, { kNONE, kNONE }, 1 },
-  // 3x
-  { kJR, { kCOND_NC, kr8 }, 2 }, { kLD, { kSP, kd16 }, 3 },
-  { kLD, { kDEREF_HL_DEC, kA }, 1 }, { kINC, { kSP, kNONE }, 1 },
-  { kINC, { kDEREF_HL, kNONE }, 1 }, { kDEC, { kDEREF_HL, kNONE }, 1 },
-  { kLD, { kDEREF_HL, kd8 }, 2 }, { kSCF, { kNONE, kNONE }, 1 },
-
-  { kJR, { kCOND_C, kr8 }, 2 }, { kADD, { kHL, kSP }, 1 },
-  { kLD, { kA, kDEREF_HL_DEC }, 1 }, { kDEC, { kSP, kNONE }, 1 },
-  { kINC, { kA, kNONE }, 1 }, { kDEC, { kA, kNONE }, 1 },
-  { kLD, { kA, kd8 }, 2 }, { kCCF, { kNONE, kNONE }, 1 },
-  // 4x
-  { kLD, { kB, kB }, 1 }, { kLD, { kB, kC }, 1 },
-  { kLD, { kB, kD }, 1 }, { kLD, { kB, kE }, 1 },
-  { kLD, { kB, kH }, 1 }, { kLD, { kB, kL }, 1 },
-  { kLD, { kB, kDEREF_HL }, 1 }, { kLD, { kB, kA }, 1 },
-
-  { kLD, { kC, kB }, 1 }, { kLD, { kC, kC }, 1 },
-  { kLD, { kC, kD }, 1 }, { kLD, { kC, kE }, 1 },
-  { kLD, { kC, kH }, 1 }, { kLD, { kC, kL }, 1 },
-  { kLD, { kC, kDEREF_HL }, 1 }, { kLD, { kC, kA }, 1 },
-  // 5x
-  { kLD, { kD, kB }, 1 }, { kLD, { kD, kC }, 1 },
-  { kLD, { kD, kD }, 1 }, { kLD, { kD, kE }, 1 },
-  { kLD, { kD, kH }, 1 }, { kLD, { kD, kL }, 1 },
-  { kLD, { kD, kDEREF_HL }, 1 }, { kLD, { kD, kA }, 1 },
-
-  { kLD, { kE, kB }, 1 }, { kLD, { kE, kC }, 1 },
-  { kLD, { kE, kD }, 1 }, { kLD, { kE, kE }, 1 },
-  { kLD, { kE, kH }, 1 }, { kLD, { kE, kL }, 1 },
-  { kLD, { kE, kDEREF_HL }, 1 }, { kLD, { kE, kA }, 1 },
-  // 6x
-  { kLD, { kH, kB }, 1 }, { kLD, { kH, kC }, 1 },
-  { kLD, { kH, kD }, 1 }, { kLD, { kH, kE }, 1 },
-  { kLD, { kH, kH }, 1 }, { kLD, { kH, kL }, 1 },
-  { kLD, { kH, kDEREF_HL }, 1 }, { kLD, { kH, kA }, 1 },
-
-  { kLD, { kL, kB }, 1 }, { kLD, { kL, kC }, 1 },
-  { kLD, { kL, kD }, 1 }, { kLD, { kL, kE }, 1 },
-  { kLD, { kL, kH }, 1 }, { kLD, { kL, kL }, 1 },
-  { kLD, { kL, kDEREF_HL }, 1 }, { kLD, { kL, kA }, 1 },
-  // 7x
-  { kLD, { kDEREF_HL, kB }, 1 }, { kLD, { kDEREF_HL, kC }, 1 },
-  { kLD, { kDEREF_HL, kD }, 1 }, { kLD, { kDEREF_HL, kE }, 1 },
-  { kLD, { kDEREF_HL, kH }, 1 }, { kLD, { kDEREF_HL, kL }, 1 },
-  { kHALT, { kNONE, kNONE }, 1 }, { kLD, { kDEREF_HL, kA }, 1 },
-
-  { kLD, { kA, kB }, 1 }, { kLD, { kA, kC }, 1 },
-  { kLD, { kA, kD }, 1 }, { kLD, { kA, kE }, 1 },
-  { kLD, { kA, kH }, 1 }, { kLD, { kA, kL }, 1 },
-  { kLD, { kA, kDEREF_HL }, 1 }, { kLD, { kA, kA }, 1 },
-  // 8x
-  { kADD, { kA, kB }, 1 }, { kADD, { kA, kC }, 1 },
-  { kADD, { kA, kD }, 1 }, { kADD, { kA, kE }, 1 },
-  { kADD, { kA, kH }, 1 }, { kADD, { kA, kL }, 1 },
-  { kADD, { kA, kDEREF_HL }, 1 }, { kADD, { kA, kA }, 1 },
-
-  { kADC, { kA, kB }, 1 }, { kADC, { kA, kC }, 1 },
-  { kADC, { kA, kD }, 1 }, { kADC, { kA, kE }, 1 },
-  { kADC, { kA, kH }, 1 }, { kADC, { kA, kL }, 1 },
-  { kADC, { kA, kDEREF_HL }, 1 }, { kADC, { kA, kA }, 1 },
-  // 9x
-  // TODO: while the kA dest is implied, maybe it makes things simpler to
-  // always list it as the first operand?
-  { kSUB, { kB, kNONE }, 1 }, { kSUB, { kC, kNONE }, 1 },
-  { kSUB, { kD, kNONE }, 1 }, { kSUB, { kE, kNONE }, 1 },
-  { kSUB, { kH, kNONE }, 1 }, { kSUB, { kL, kNONE }, 1 },
-  { kSUB, { kDEREF_HL, kNONE }, 1 }, { kSUB, { kA, kNONE }, 1 },
-
-  { kSBC, { kA, kB }, 1 }, { kSBC, { kA, kC }, 1 },
-  { kSBC, { kA, kD }, 1 }, { kSBC, { kA, kE }, 1 },
-  { kSBC, { kA, kH }, 1 }, { kSBC, { kA, kL }, 1 },
-  { kSBC, { kA, kDEREF_HL }, 1 }, { kSBC, { kA, kA }, 1 },
-  // Ax
-  { kAND, { kB, kNONE }, 1 }, { kAND, { kC, kNONE }, 1 },
-  { kAND, { kD, kNONE }, 1 }, { kAND, { kE, kNONE }, 1 },
-  { kAND, { kH, kNONE }, 1 }, { kAND, { kL, kNONE }, 1 },
-  { kAND, { kDEREF_HL, kNONE }, 1 }, { kAND, { kA, kNONE }, 1 },
-
-  { kXOR, { kB, kNONE }, 1 }, { kXOR, { kC, kNONE }, 1 },
-  { kXOR, { kD, kNONE }, 1 }, { kXOR, { kE, kNONE }, 1 },
-  { kXOR, { kH, kNONE }, 1 }, { kXOR, { kL, kNONE }, 1 },
-  { kXOR, { kDEREF_HL, kNONE }, 1 }, { kXOR, { kA, kNONE }, 1 },
-  // Bx
-  { kOR, { kB, kNONE }, 1 }, { kOR, { kC, kNONE }, 1 },
-  { kOR, { kD, kNONE }, 1 }, { kOR, { kE, kNONE }, 1 },
-  { kOR, { kH, kNONE }, 1 }, { kOR, { kL, kNONE }, 1 },
-  { kOR, { kDEREF_HL, kNONE }, 1 }, { kOR, { kA, kNONE }, 1 },
-
-  { kCP, { kB, kNONE }, 1 }, { kCP, { kC, kNONE }, 1 },
-  { kCP, { kD, kNONE }, 1 }, { kCP, { kE, kNONE }, 1 },
-  { kCP, { kH, kNONE }, 1 }, { kCP, { kL, kNONE }, 1 },
-  { kCP, { kDEREF_HL, kNONE }, 1 }, { kCP, { kA, kNONE }, 1 },
-  // Cx
-  { kRET, { kCOND_NZ, kNONE }, 1 }, { kPOP, { kBC, kNONE }, 1 },
-  { kJP, { kCOND_NZ, ka16 }, 3 }, { kJP, { kNONE, ka16 }, 3 },
-  { kCALL, { kCOND_NZ, ka16 }, 3 }, { kPUSH , { kBC, kNONE }, 1 },
-  { kADD, { kA, kd8 }, 2 }, { kRST, { kNONE, kNONE }, 1 },
-
-  { kRET, { kCOND_Z, kNONE }, 1 }, { kRET, { kNONE, kNONE }, 1 },
-  { kJP, { kCOND_Z, ka16 }, 3 }, { kCB, { kNONE, kNONE }, 2 },
-  { kCALL, { kCOND_Z, ka16 }, 3 }, { kCALL, { kNONE, ka16 }, 3 },
-  { kADC, { kA, kd8 }, 2 }, { kRST, { kNONE, kNONE }, 1 },
-  // Dx
-  { kRET, { kCOND_NC, kNONE }, 1 }, { kPOP, { kDE, kNONE }, 1 },
-  { kJP, { kCOND_NC, ka16 }, 3 }, { kInvalid, { kNONE, kNONE }, 1 },
-  { kCALL, { kCOND_NC, ka16 }, 3 }, { kPUSH, { kDE, kNONE }, 1 },
-  { kSUB, { kNONE, kd8 }, 2 }, { kRST, { kNONE, kNONE }, 1 },
-
-  { kRET, { kCOND_C, kNONE }, 1 }, { kRETI, { kNONE, kNONE }, 1 },
-  { kJP, { kCOND_C, ka16 }, 3 }, { kInvalid, { kNONE, kNONE }, 1 },
-  { kCALL, { kCOND_C, ka16, }, 3 }, { kInvalid, { kNONE, kNONE }, 1 },
-  { kSBC, { kA, kd8 }, 2 }, { kRST, { kNONE, kNONE }, 1 },
-  // Ex
-  { kLD, { kDEREF_a8, kA }, 2 }, { kPOP, { kHL, kNONE }, 1 },
-  { kLD, { kDEREF_C, kA }, 1 }, { kInvalid, { kNONE, kNONE }, 1 },
-  { kInvalid, { kNONE, kNONE }, 1 }, { kPUSH, { kHL, kNONE }, 1 },
-  { kAND, { kd8, kNONE }, 2 }, { kRST, { kNONE, kNONE }, 1 },
-
-  { kADD, { kSP, kr8 }, 2 }, { kJP, { kNONE, kDEREF_HL }, 2 },
-  { kLD, { kDEREF_a16, kA }, 3 }, { kInvalid, { kNONE, kNONE }, 1 },
-  { kInvalid, { kNONE, kNONE }, 1 }, { kInvalid, { kNONE, kNONE }, 1 },
-  { kXOR, { kd8, kNONE }, 1 }, { kRST, { kNONE, kNONE }, 1 },
-  // Fx
-  { kLD, { kA, kDEREF_a8 }, 2 }, { kPOP, { kAF, kNONE }, 1 },
-  { kLD, { kA, kDEREF_C }, 1 }, { kDI, { kNONE, kNONE }, 1 },
-  { kInvalid, { kNONE, kNONE }, 1 }, { kPUSH, { kAF, kNONE }, 1 },
-  { kOR, { kd8, kNONE }, 2 }, { kRST, { kNONE, kNONE }, 1 },
-
-  { kLD, { kHL, kSP_r8}, 2 }, { kLD, { kSP, kHL }, 1 },
-  { kLD, { kA, kDEREF_a16 }, 3 }, { kEI, { kNONE, kNONE }, 1 },
-  { kInvalid, { kNONE, kNONE }, 1 }, { kInvalid, { kNONE, kNONE }, 1 },
-  { kCP, { kd8, kNONE }, 2 }, { kRST, { kNONE, kNONE }, 1 },
-};
-
 // the pc should be pointing at prefix (0xCB) + 1
 void decode_cb (const struct rom* const rom, const uint16_t pc,
     struct instruction* const instruction) {
@@ -766,7 +599,7 @@ void decode_instruction (const struct rom* const rom, const uint16_t pc,
     instruction->length = 1;
   } else
 #endif
-  if (instruction->opcode == kCB) {
+    if (instruction->opcode == kCB) {
     decode_cb(rom, pc + 1, instruction);
   } else {
     instruction->operands[0] = operand_0_table[first_byte];
@@ -812,20 +645,13 @@ int disassemble (const struct rom* const rom) {
   struct instruction instruction;
   while (pc < rom->size) {
     decode_instruction(rom, pc, &instruction);
-    assert(instruction.length > 0);
-    assert(instruction.length < 4);
     printf(PRIshort ": ", pc);
     print_instruction(&instruction);
+    assert(instruction.length > 0);
+    assert(instruction.length < 4);
     pc += instruction.length;
   }
   return 0;
-}
-
-static void print_sizes () {
-  printf(
-      "sizeof struct instruction: %lu, sizeof opcode: %lu, "
-      "sizeof operand: %lu\n",
-      sizeof(struct instruction), sizeof(enum Opcode), sizeof(enum Operand));
 }
 
 int main (int argc, char** argv) {
@@ -833,7 +659,6 @@ int main (int argc, char** argv) {
     fprintf(stderr, "Usage: ./disassembler <rom.gb>\n");
     return -1;
   }
-  print_sizes();
 
   const char* const rom_fname = argv[1];
   printf("opening %s\n", rom_fname);
