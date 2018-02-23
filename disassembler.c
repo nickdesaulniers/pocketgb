caa469537049de07895e68ecae3ee743c7691458
Nick Desaulniers <nick.desaulniers@gmail.com>
2018-02-22 23:05:33 -0800

    Revert "disassembler: use shiny new interleaved instruction table. Need to handle CB"
    This reverts commit 00d474b8c51053f26312e632c61346491470b166.

--- disassembler.c
+++ disassembler.c
@@ -165,6 +165,57 @@ static const char* const opcode_str_table [] = {
   "SET"
 };
 
+static const enum Opcode decode_table [256] = {
+  // 0x
+  kNOP, kLD, kLD, kINC, kINC, kDEC, kLD, kRLC,
+  kLD, kADD, kLD, kDEC, kINC, kDEC, kLD, kRRC,
+  // 1x
+  kSTOP, kLD, kLD, kINC, kINC, kDEC, kLD, kRL,
+  kJR, kADD, kLD, kDEC, kINC, kDEC, kLD, kRR,
+  // 2x
+  kJR, kLD, kLD, kINC, kINC, kDEC, kLD, kDAA,
+  kJR, kADD, kLD, kDEC, kINC, kDEC, kLD, kCPL,
+  // 3x
+  kJR, kLD, kLD, kINC, kINC, kDEC, kLD, kSCF,
+  kJR, kADD, kLD, kDEC, kINC, kDEC, kLD, kCCF,
+  // 4x
+  kLD, kLD, kLD, kLD, kLD, kLD, kLD, kLD,
+  kLD, kLD, kLD, kLD, kLD, kLD, kLD, kLD,
+  // 5x
+  kLD, kLD, kLD, kLD, kLD, kLD, kLD, kLD,
+  kLD, kLD, kLD, kLD, kLD, kLD, kLD, kLD,
+  // 6x
+  kLD, kLD, kLD, kLD, kLD, kLD, kLD, kLD,
+  kLD, kLD, kLD, kLD, kLD, kLD, kLD, kLD,
+  // 7x
+  kLD, kLD, kLD, kLD, kLD, kLD, kHALT, kLD,
+  kLD, kLD, kLD, kLD, kLD, kLD, kLD, kLD,
+  // 8x
+  kADD, kADD, kADD, kADD, kADD, kADD, kADD, kADD,
+  kADC, kADC, kADC, kADC, kADC, kADC, kADC, kADC,
+  // 9x
+  kSUB, kSUB, kSUB, kSUB, kSUB, kSUB, kSUB, kSUB,
+  kSBC, kSBC, kSBC, kSBC, kSBC, kSBC, kSBC, kSBC,
+  // Ax
+  kAND, kAND, kAND, kAND, kAND, kAND, kAND, kAND,
+  kXOR, kXOR, kXOR, kXOR, kXOR, kXOR, kXOR, kXOR,
+  // Bx
+  kOR, kOR, kOR, kOR, kOR, kOR, kOR, kOR,
+  kCP, kCP, kCP, kCP, kCP, kCP, kCP, kCP,
+  // Cx
+  kRET, kPOP, kJP, kJP, kCALL, kPUSH, kADD, kRST,
+  kRET, kRET, kJP, kCB, kCALL, kCALL, kADC, kRST,
+  // Dx
+  kRET, kPOP, kJP, kInvalid, kCALL, kPUSH, kSUB, kRST,
+  kRET, kRET, kJP, kInvalid, kCALL, kInvalid, kSBC, kRST,
+  // Ex
+  kLD, kPOP, kLD, kInvalid, kInvalid, kPUSH, kAND, kRST,
+  kADD, kJP, kLD, kInvalid, kInvalid, kInvalid, kXOR, kRST,
+  // Fx
+  kLD, kPOP, kLD, kDI, kInvalid, kPUSH, kOR, kRST,
+  kLD, kLD, kLD, kEI, kInvalid, kInvalid, kCP, kRST
+};
+
 static const enum Opcode cb_decode_table [256] = {
   // 0x
   kRLC, kRLC, kRLC, kRLC, kRLC, kRLC, kRLC, kRLC,
@@ -313,6 +364,109 @@ static const char* const operand_str_table [] = {
   "d16",
   "a16",
   "(a16)"
+};
+
+static const enum Operand operand_0_table [256] = {
+  // 0x
+  kNONE, kBC, kDEREF_BC, kBC, kB, kB, kB, kNONE,
+  kDEREF_a16, kHL, kA, kBC, kC, kC, kC, kNONE,
+  // 1x
+  kNONE, kDE, kDEREF_DE, kDE, kD, kD, kD, kNONE,
+  kNONE, kHL, kA, kDE, kE, kE, kE, kNONE,
+  // 2x
+  kCOND_NZ, kHL, kDEREF_HL_INC, kHL, kH, kH, kH, kNONE,
+  kCOND_Z, kHL, kA, kHL, kL, kL, kL, kNONE,
+  // 3x
+  kCOND_NC, kSP, kDEREF_HL_DEC, kSP, kDEREF_HL, kDEREF_HL, kDEREF_HL, kNONE,
+  kCOND_C, kHL, kA, kSP, kA, kA, kA, kNONE,
+  // 4x
+  kB, kB, kB, kB, kB, kB, kB, kB,
+  kC, kC, kC, kC, kC, kC, kC, kC,
+  // 5x
+  kD, kD, kD, kD, kD, kD, kD, kD,
+  kE, kE, kE, kE, kE, kE, kE, kE,
+  // 6x
+  kH, kH, kH, kH, kH, kH, kH, kH,
+  kL, kL, kL, kL, kL, kL, kL, kL,
+  // 7x
+  kDEREF_HL, kDEREF_HL, kDEREF_HL, kDEREF_HL,
+  kDEREF_HL, kDEREF_HL, kNONE, kDEREF_HL,
+  kA, kA, kA, kA, kA, kA, kA, kA,
+  // 8x
+  kA, kA, kA, kA, kA, kA, kA, kA,
+  kA, kA, kA, kA, kA, kA, kA, kA,
+  // 9x
+  kB, kC, kD, kE, kH, kL, kDEREF_HL, kA,
+  kA, kA, kA, kA, kA, kA, kA, kA,
+  // Ax
+  kB, kC, kD, kE, kH, kL, kDEREF_HL, kA,
+  kB, kC, kD, kE, kH, kL, kDEREF_HL, kA,
+  // Bx
+  kB, kC, kD, kE, kH, kL, kDEREF_HL, kA,
+  kB, kC, kD, kE, kH, kL, kDEREF_HL, kA,
+  // Cx
+  kCOND_NZ, kBC, kCOND_NZ, kNONE, kCOND_NZ, kBC, kA, kNONE,
+  kNONE, kNONE, kCOND_Z, kNONE, kCOND_Z, kNONE, kA, kNONE,
+  // Dx
+  kCOND_NC, kDE, kCOND_NC, kNONE, kCOND_NZ, kDE, kNONE, kNONE,
+  kNONE, kNONE, kCOND_C, kNONE, kNONE, kNONE, kA, kNONE,
+  // Ex
+  kDEREF_a8, kHL, kDEREF_C, kNONE, kNONE, kHL, kd8, kNONE,
+  kSP, kDEREF_HL, kDEREF_a16, kNONE, kNONE, kNONE, kd8, kNONE,
+  // Fx
+  kA, kAF, kA, kNONE, kNONE, kAF, kd8, kNONE,
+  kHL, kSP, kA, kNONE, kNONE, kNONE, kd8, kNONE
+};
+
+static const enum Operand operand_1_table [256] = {
+  // 0x
+  kNONE, kd16, kA, kNONE, kNONE, kNONE, kd8, kNONE,
+  kSP, kBC, kDEREF_BC, kNONE, kNONE, kNONE, kd8, kNONE,
+  // 1x
+  kNONE, kd16, kA, kNONE, kNONE, kNONE, kd8, kNONE,
+  kr8, kDE, kDEREF_DE, kNONE, kNONE, kNONE, kd8, kNONE,
+  // 2x
+  kr8, kd16, kA, kNONE, kNONE, kNONE, kd8, kNONE,
+  kr8, kHL, kDEREF_HL_INC, kNONE, kNONE, kNONE, kd8, kNONE,
+  // 3x
+  kr8, kd16, kA, kNONE, kNONE, kNONE, kd8, kNONE,
+  kr8, kSP, kDEREF_HL_DEC, kNONE, kNONE, kNONE, kd8, kNONE,
+  // 4x
+  kB, kC, kD, kE, kH, kL, kDEREF_HL, kA,
+  kB, kC, kD, kE, kH, kL, kDEREF_HL, kA,
+  // 5x
+  kB, kC, kD, kE, kH, kL, kDEREF_HL, kA,
+  kB, kC, kD, kE, kH, kL, kDEREF_HL, kA,
+  // 6x
+  kB, kC, kD, kE, kH, kL, kDEREF_HL, kA,
+  kB, kC, kD, kE, kH, kL, kDEREF_HL, kA,
+  // 7x
+  kB, kC, kD, kE, kH, kL, kNONE, kA,
+  kB, kC, kD, kE, kH, kL, kDEREF_HL, kA,
+  // 8x
+  kB, kC, kD, kE, kH, kL, kDEREF_HL, kA,
+  kB, kC, kD, kE, kH, kL, kDEREF_HL, kA,
+  // 9x
+  kNONE, kNONE, kNONE, kNONE, kNONE, kNONE, kNONE, kNONE,
+  kB, kC, kD, kE, kH, kL, kDEREF_HL, kA,
+  // Ax
+  kNONE, kNONE, kNONE, kNONE, kNONE, kNONE, kNONE, kNONE,
+  kNONE, kNONE, kNONE, kNONE, kNONE, kNONE, kNONE, kNONE,
+  // Bx
+  kNONE, kNONE, kNONE, kNONE, kNONE, kNONE, kNONE, kNONE,
+  kNONE, kNONE, kNONE, kNONE, kNONE, kNONE, kNONE, kNONE,
+  // Cx
+  kNONE, kNONE, ka16, ka16, ka16, kNONE, kd8, kNONE,
+  kNONE, kNONE, ka16, kNONE, ka16, ka16, kd8, kNONE,
+  // Dx
+  kNONE, kNONE, ka16, kNONE, ka16, kNONE, kNONE, kNONE,
+  kNONE, kNONE, ka16, kNONE, ka16, kNONE, kd8, kNONE,
+  // Ex
+  kA, kNONE, kA, kNONE, kNONE, kNONE, kNONE, kNONE,
+  kr8, kDEREF_HL, kA, kNONE, kNONE, kNONE, kNONE, kNONE,
+  // Fx
+  kDEREF_a8, kNONE, kA, kNONE, kNONE, kNONE, kNONE, kNONE,
+  kSP_r8, kHL, kDEREF_a16, kNONE, kNONE, kNONE, kNONE, kNONE
 };
 
 static const enum Operand cb_operand_0_table [256] = {
@@ -600,17 +754,31 @@ void decode_cb (const struct rom* const rom, const uint16_t pc,
 }
 
 void decode_instruction (const struct rom* const rom, const uint16_t pc,
-    const struct instruction** const instruction) {
+    struct instruction* const instruction) {
+  memset(instruction, 0, sizeof(*instruction));
   const uint8_t first_byte = rom->data[pc];
-  *instruction = &instruction_table[first_byte];
-  /*if (instruction->opcode == kCB) {*/
+  instruction->opcode = decode_table[first_byte];
 #ifndef NDEBUG
-  if ((*instruction)->opcode == kInvalid) {
+  if (instruction->opcode == kInvalid) {
     fprintf(stderr, "Looks like were trying to decode data as instructions,\n"
         "rest of disassembly may be invalid from this point.\n WARN: "
         PRIshort "\n", pc);
-  }
+    instruction->length = 1;
+  } else
 #endif
+  if (instruction->opcode == kCB) {
+    decode_cb(rom, pc + 1, instruction);
+  } else {
+    instruction->operands[0] = operand_0_table[first_byte];
+    instruction->operands[1] = operand_1_table[first_byte];
+    // calculate instruction length
+    const uint8_t len = 1 +
+      (instruction->operands[0] > kINSTRUCTION_LENGTH_0) +
+      (instruction->operands[0] > kINSTRUCTION_LENGTH_1) +
+      (instruction->operands[1] > kINSTRUCTION_LENGTH_0) +
+      (instruction->operands[1] > kINSTRUCTION_LENGTH_1);
+    instruction->length = len;
+  }
 }
 
 // TODO: maybe make this some kind of ToString() like fn?
@@ -641,14 +809,14 @@ void print_instruction (const struct instruction* const instruction) {
 
 int disassemble (const struct rom* const rom) {
   uint16_t pc = 0;
-  const struct instruction* instruction = NULL;
+  struct instruction instruction;
   while (pc < rom->size) {
     decode_instruction(rom, pc, &instruction);
-    assert(instruction->length > 0);
-    assert(instruction->length < 4);
+    assert(instruction.length > 0);
+    assert(instruction.length < 4);
     printf(PRIshort ": ", pc);
-    print_instruction(instruction);
-    pc += instruction->length;
+    print_instruction(&instruction);
+    pc += instruction.length;
   }
   return 0;
 }
